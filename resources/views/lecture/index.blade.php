a5b9c9a48d7058729545b36872e11e50967fa395
Tim Jossund <timjossund@gmail.com>
2025-11-18 13:55:17 -0600

    push

--- resources/views/lecture/index.blade.php
+++ resources/views/lecture/index.blade.php
@@ -2,9 +2,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Lecture: {{ $lecture->title }}
-                    <p>{{ $lecture->description }}</p>
+                <div class="p-6 text-white bg-gray-800">
+                    <h1 class="text-4xl font-black">Lecture: {{ $lecture->name }}</h1>
+                    <p class="text-gray-600">{{ $lecture->description }}</p>
                 </div>
             </div>
         </div>
--- resources/views/lecture/single.blade.php
+++ resources/views/lecture/single.blade.php
@@ -2,8 +2,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Lecture: {{ $lecture->name }}
+                <div class="p-6 text-white bg-gray-900">
+                    <h2 class="text-2xl font-black">Lecture: {{ $lecture->name }}</h2>
                 </div>
                 <div class="p-6 text-gray-900">
                     <p>{{ $lecture->description }}</p>
