be68c8fa07ae30cb60102d2bb9e77546bb4510f0
Tim Jossund <timjossund@gmail.com>
2026-02-05 11:14:28 -0600

    adding some new stuff

---
+++ app/Models/TestTracker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TestTracker extends Model
+{
+    protected $fillable = [
+        'test_id',
+        'user_id',
+        'score_id',
+        'completed',
+    ];
+
+    public function testTracked(): BelongsTo
+    {
+        return $this->belongsTo(Test::class);
+    }
+
+    public function userTracked(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scoreTracked(): BelongsTo
+    {
+        return $this->belongsTo(TestScore::class);
+    }
+}
---
+++ database/migrations/2026_02_05_135757_create_tests_taken_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tests_taken', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('test_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('score_id')->constrained()->cascadeOnDelete();
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tests_taken');
+    }
+};
